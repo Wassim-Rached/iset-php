18c4d28c7c9745d04d2303d197cb60efd6e4062a
Wassim-Rached <wassimrached404@gmail.com>
2023-11-23 09:19:33 +0100

    Update stylesheet and script paths

--- tp7/Editeurs/index.php
+++ tp7/Editeurs/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Editeurs</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../style.css" />
   </head>
   <body>
     <div class="container">
@@ -18,6 +18,6 @@
       </li>
     </ul>
     </div>
-    <script src="./index.js"></script>
+    <script src="../index.js"></script>
   </body>
 </html>
