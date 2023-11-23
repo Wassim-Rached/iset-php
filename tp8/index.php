8967ba4421a75c46b3605ef2ab7307690327dcfb
Wassim-Rached <wassimrached404@gmail.com>
2023-11-23 09:19:47 +0100

    Add login form to index.php

---
+++ tp8/index.php
@@ -0,0 +1,7 @@
+<form method="post" action="./login.php">
+	<table>
+	<tr><td>Login :</td> <td><input type="text" name="login" size="11"/></td></tr>
+	<tr><td>Mot de passe :</td> <td><input type="password" name="mp" size="11"/><br/></td></tr>
+	<tr><td colspan="2"><center><input type="submit" value="se connecter"/></center></td></tr>
+	</table>
+</form>
