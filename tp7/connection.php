ba7049f57884743c1581bb8c9e9181163fc4b075
Wassim-Rached <wassimrached404@gmail.com>
2023-11-23 09:19:21 +0100

    Add connection status message

--- tp7/connection.php
+++ tp7/connection.php
@@ -2,6 +2,7 @@
 
 	try {
 		$cnx = new PDO('mysql:host=localhost:6033;dbname=gugugaga','wassim','password');
+		echo "connected";
 	} catch (Exception $e) {
 		echo "error : ".$e->getMessage();
 		echo "code : ".$e->getCode();
